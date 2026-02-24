2abb2b75a0abc3fb19110fee5080d23f9c8d75a6
Ananya Kundarapu <ananya.kundarapu@egen.ai>
2026-02-24 19:55:58 +0530

    Fix Cloud Run port binding

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,10 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(
         "main:app",
-        host="localhost",
-        port=8000,
-        reload=True
+        host="0.0.0.0",
+        port=port
     )
