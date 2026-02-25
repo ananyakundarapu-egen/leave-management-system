f1a96f942a622b50a0e7b8707629b4816627adc8
Ananya Kundarapu <ananya.kundarapu@egen.ai>
2026-02-25 11:32:49 +0530

    Fix API baseUrl for Cloud Run

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-private baseUrl = '/api';
+  private baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   applyLeave(data: any) {
