dfa9cb2898ffd7bfce0916df9291656a270d242d
Ananya Kundarapu <ananya.kundarapu@egen.ai>
2026-02-25 14:25:30 +0530

    Remove unused firebase_verify file

--- backend/firebase_admin_init.py
+++ backend/firebase_admin_init.py
@@ -1,5 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
 
 if not firebase_admin._apps:
-    firebase_admin.initialize_app()
+    if os.getenv("GOOGLE_CLOUD_RUN"):
+        firebase_admin.initialize_app()
+    else:
+        cred = credentials.Certificate("firebase_key.json")
+        firebase_admin.initialize_app(cred)
