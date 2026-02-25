efd0b54670450c45abaad5492138c199e0f11252
Ananya Kundarapu <ananya.kundarapu@egen.ai>
2026-02-25 14:39:50 +0530

    Fix Firebase init for Cloud Run

--- backend/firebase_admin_init.py
+++ backend/firebase_admin_init.py
@@ -3,8 +3,11 @@ from firebase_admin import credentials
 import os
 
 if not firebase_admin._apps:
-    if os.getenv("GOOGLE_CLOUD_RUN"):
+    try:
+        if os.path.exists("firebase_key.json"):
+            cred = credentials.Certificate("firebase_key.json")
+            firebase_admin.initialize_app(cred)
+        else:
+            firebase_admin.initialize_app()
+    except Exception:
         firebase_admin.initialize_app()
-    else:
-        cred = credentials.Certificate("firebase_key.json")
-        firebase_admin.initialize_app(cred)
